8df749b3ffb620d83df89c860975c1ad8e836036
Thiago Aragão <thiago.alpha.06@gmail.com>
2024-08-27 14:00:59 -0300

    Initiating with Pandas

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-import numpy as np
-import matplotlib.pyplot as plt
+import pandas as pd
 
-# Generate the array with random values between -1 and 1
-arr = np.random.random((1000, 2))
-arr = (arr * 2) - 1
+# https://raw.githubusercontent.com/lazyprogrammer/machine_learning_examples/master/tf2.0/sbux.csv
 
-# Create an array for colors based on conditions:
-# - If both x and y are positive or both are negative, color = 1 (e.g., blue)
-# - If one is positive and the other is negative, color = 0 (e.g., red)
+csv = pd.read_csv('https://raw.githubusercontent.com/lazyprogrammer/machine_learning_examples/master/tf2.0/sbux.csv')
 
-colors = np.where((arr[:, 0] > 0) & (arr[:, 1] > 0) | (arr[:, 0] < 0) & (arr[:, 1] < 0), 1, 0)
-
-# Create a scatter plot using the color array
-plt.scatter(arr[:, 0], arr[:, 1], c=colors, cmap='bwr')  # 'bwr' colormap for red/blue
-plt.show()
+print(csv.info())
