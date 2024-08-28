3fc76210881212a08701698a373544f89a389ecc
Thiago Aragão <thiago.alpha.06@gmail.com>
2024-08-28 17:35:42 -0300

    Finished preparations for TensorFlow

---
+++ __main__.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# https://raw.githubusercontent.com/lazyprogrammer/machine_learning_examples/master/tf2.0/sbux.csv
+
+
+def main():
+    dwdw
+
+
+if __name__=='__main__':
+    main()
--- main.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# https://raw.githubusercontent.com/lazyprogrammer/machine_learning_examples/master/tf2.0/sbux.csv
-
-csv = pd.read_csv('https://raw.githubusercontent.com/lazyprogrammer/machine_learning_examples/master/tf2.0/sbux.csv')
-
-print(csv.info())
