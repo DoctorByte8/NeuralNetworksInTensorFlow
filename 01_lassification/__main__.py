44d8df260133d46f6b0afc5867106714e6a3922e
Thiago Aragão <thiago.alpha.06@gmail.com>
2024-10-12 16:45:50 -0300

    save

--- 01_lassification/__main__.py
+++ 01_lassification/__main__.py
@@ -1,6 +1,6 @@
 from sklearn import datasets, model_selection, preprocessing
 import numpy as np
-#import tensorflow as tf
+import tensorflow as tf
 
 print("\n\n")
 
@@ -8,8 +8,23 @@ data = datasets.load_breast_cancer()
 
 X_treino, X_teste, Y_treino, Y_teste = model_selection.train_test_split(data.data, data.target, test_size=0.33)
 
-X_treino = preprocessing.StandardScaler.fit_transform(X_treino)
-Y_treino = preprocessing.StandardScaler.fit_transform(Y_treino)
-X_teste = preprocessing.StandardScaler.transform(X_teste)
+scaler = preprocessing.StandardScaler()
+X_treino = scaler.fit_transform(X_treino)
+X_teste = scaler.transform(X_teste)
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Input(shape=(D,)),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(
+    optimizer='adam',
+    loss='binary_crossentropy',
+    metrics=['accuracy']
+)
+
+r = model.fit(X_treino, Y_treino, validation_data=(X_teste, Y_teste), epochs=100)
+
+print(r)
 
 print("\n\n")
